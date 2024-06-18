09198fa210a7f3a18b16e7114046056f3c0450ff
arifhussain1313 <zakiriinguti@gmail.com>
2024-06-17 23:16:31 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// var username;
+// var myname = "Arif Hussain";
+// console.log(myname);
+// var message = "Hello World";
+// alert(message);
+// // Assume student data
+// const studentName = "John Doe";
+// const studentAge = 20;
+// const studentMajor = "Computer Science";
+
+// // Display student data in alert boxes
+// alert(`Student Name: ${studentName}`);
+// alert(`Student Age: ${studentAge}`);
+// alert(`Student Major: ${studentMajor}`);
+
+var rows = 5
+var cols = 5
+
+for(let i = 1; i<= rows; i++){
+    for(let j = 1 <= cols; j++){
+        document.write("pizza");
+    }
+   
+}
