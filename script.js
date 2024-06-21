f8d4fbbca13632d6ec975d7df4fd2be4a4f578c1
arifhussain1313 <zakiriinguti@gmail.com>
2024-06-20 21:53:48 -0700

    pizza triangle

--- script.js
+++ script.js
@@ -13,12 +13,53 @@
 // alert(`Student Age: ${studentAge}`);
 // alert(`Student Major: ${studentMajor}`);
 
-var rows = 5
-var cols = 5
+// var rows = 5
+// var cols = 5
 
-for(let i = 1; i<= rows; i++){
-    for(let j = 1 <= cols; j++){
-        document.write("pizza");
-    }
+// for(let i = 1; i<= rows; i++){
+//     for(let j = 1 <= cols; j++){
+//         document.write("pizza");
+//     }
    
+// };
+
+// const word = "pizza";
+// for (let i = word; i >= 1; i--) {
+//     const words = "pizza".repeat(i);
+// }
+// alert(triangle);
+// console.log(triangle);
+// console.log(word);
+
+// Define the number of rows for the inverted triangle
+// const numRows = 5;
+
+// // Loop through each row in reverse order
+// for (let i = numRows; i >= 1; i--) {
+//     // Create a string with pizza words (e.g., "🍕🍕🍕")
+//     const pizzaSlice = "pizza".repeat(i);
+//     console.log(pizzaSlice);
+// }
+// al
+
+
+
+
+// var word = "pizza";
+// for (let i = 5; i => 1; i--){
+//     for (let j = 1; j => 1; j--){
+//         document.write("pizza");
+//     }
+//     document.write("<br>");
+// alert(word);
+// };
+
+function triangle(n) {
+    let i = n; i >=1; i--{
+        let row ='';
+        for (let j = 1; j <= i; j++) {
+            row += 'pizza';
+        }
+        console.log(row);
+    }
 }
